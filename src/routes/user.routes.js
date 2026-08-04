8d2783c4c906f8456bd0c6c8d5d59c2d44f654bf
Tajinder Singh <tajinders@futuresoftindia.com>
2026-08-04 12:05:17 +0530

    auth middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,5 +4,7 @@ import { registerUser } from "../controllers/user.controller.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", verifyJWT, logoutUser);
 
 export default router;
