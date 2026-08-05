61d9fcfdb862f4e884c576a0f76d15a34fd94561
Tajinder Singh <tajinders@futuresoftindia.com>
2026-08-05 10:42:16 +0530

    refresh token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", verifyJWT, logoutUser);
+router.post("/refresh-token", refreshAccessToken);
 
 export default router;
